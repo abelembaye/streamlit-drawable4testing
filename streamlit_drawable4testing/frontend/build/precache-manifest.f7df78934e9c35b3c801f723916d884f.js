e5fed1bd31d918d3fe00329ac2cc44f056fc45e0
abelembaye <abel.embaye@gmail.com>
2024-01-06 09:31:15 -0600

    initial commit

---
+++ streamlit_drawable4testing/frontend/build/precache-manifest.f7df78934e9c35b3c801f723916d884f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac09c34f45264d06590d3ecc51b57c17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eaa911bedf27acaeb467",
+    "url": "./static/css/main.b38bd919.chunk.css"
+  },
+  {
+    "revision": "e22026a7903ec0dd82ba",
+    "url": "./static/js/2.45d09d04.chunk.js"
+  },
+  {
+    "revision": "eaa911bedf27acaeb467",
+    "url": "./static/js/main.207f9c21.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
